59e634dff99606ac6b12117902afdf3bee9d0acc
SammyMa <sma990404@gmail.com>
2025-04-03 21:35:47 -0700

    add new project: shopping app

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,45 +1,51 @@
 import React from "react";
 import "./Project.scss";
-import vueMemoryGame from '../../image/vue_memory_game.jpg';
-import smMedia from '../../image/sm_media.png'
+import vueMemoryGame from "../../image/vue_memory_game.jpg";
+import smMedia from "../../image/sm_media.png";
+import shoppingApp from "../../image/shopping_app.png";
 
 const projects = [
-    {
-        title: 'Simple Social Media',
-        img: smMedia,
-        url: 'https://ykzk-nextjs-social-media.vercel.app/'
-    },
-    {
-        title: 'Vue Memory game',
-        img: vueMemoryGame,
-        url: 'https://yokkazuki.github.io/vue-memory-game'
-    },
-]
+  {
+    title: "Shopping Website",
+    img: shoppingApp,
+    url: "https://ykzk-wannagoods.vercel.app/",
+  },
+  {
+    title: "Simple Social Media",
+    img: smMedia,
+    url: "https://ykzk-nextjs-social-media.vercel.app/",
+  },
+  {
+    title: "Vue Memory game",
+    img: vueMemoryGame,
+    url: "https://yokkazuki.github.io/vue-memory-game",
+  },
+];
 
 export default function Project() {
+  return (
+    <div className="project" id="project">
+      <div className="header">project</div>
 
-    return <div className="project" id="project">
-
-        <div className="header">
-            project
-        </div>
-
-        <div className="project-list">
-            {projects.map(project => {
-                return (
-                    <a href={project.url} target={"_blank"} rel="noreferrer" key={project.title}>
-                        <div className="project-card">
-                            <div className="image-container">
-                                <img src={project.img} alt="" />
-                            </div>
-                            <div className="title">{project.title}</div>
-                        </div>
-                    </a>
-                )
-            })}
-        </div>
+      <div className="project-list">
+        {projects.map((project) => {
+          return (
+            <a
+              href={project.url}
+              target={"_blank"}
+              rel="noreferrer"
+              key={project.title}
+            >
+              <div className="project-card">
+                <div className="image-container">
+                  <img src={project.img} alt="" />
+                </div>
+                <div className="title">{project.title}</div>
+              </div>
+            </a>
+          );
+        })}
+      </div>
     </div>
-
-
-
+  );
 }
