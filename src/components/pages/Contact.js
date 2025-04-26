44b45248b4abd07296d6ba90ceb9c82ca23d9d5e
SammyMa <sma990404@gmail.com>
2025-04-26 15:42:59 -0700

    add canada tel no.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,28 +1,29 @@
-import React from "react"
-import "./Contact.scss"
-import { AiFillMail } from "react-icons/ai"
-import { BsFillTelephoneFill } from "react-icons/bs"
+import React from "react";
+import "./Contact.scss";
+import { AiFillMail } from "react-icons/ai";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
 export default function Contact() {
-    return <div className="contact" id="contact">
-        <div className="header">
-            Contact me
+  return (
+    <div className="contact" id="contact">
+      <div className="header">Contact me</div>
+      <div className="contact-list">
+        <div className="contact-info">
+          <BsFillTelephoneFill className="contact-icon" size="25px" />
+          <div className="content">
+            <div className="title">Phone</div>
+            <div className="subtitle">{`+852 55418126`}</div>
+            <div className="subtitle">{`+1 (778)873-1101`}</div>
+          </div>
         </div>
-        <div className="contact-list">
-            <div className="contact-info">
-                <BsFillTelephoneFill className="contact-icon" size="25px" />
-                <div className="content">
-                    <div className="title">Phone</div>
-                    <span className="subtitle">5541 8126</span>
-                </div>
-            </div>
-            <div className="contact-info">
-                <AiFillMail className="contact-icon" size="25px" />
-                <div className="content">
-                    <div className="title">Email</div>
-                    <span className="subtitle">sma990404@gmail.com</span>
-                </div>
-            </div>
+        <div className="contact-info">
+          <AiFillMail className="contact-icon" size="25px" />
+          <div className="content">
+            <div className="title">Email</div>
+            <span className="subtitle">sma990404@gmail.com</span>
+          </div>
         </div>
+      </div>
     </div>
+  );
 }
